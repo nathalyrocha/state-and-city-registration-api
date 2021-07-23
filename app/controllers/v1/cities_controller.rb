d552ca707226174decc9d1e54d2b8d72219b619f
Náthaly Rocha <nathalyfroliveira@gmail.com>
2021-07-22 23:08:20 -0300

    Add cities controller

---
+++ app/controllers/v1/cities_controller.rb
@@ -0,0 +1,21 @@
+class V1::CitiesController < ApplicationController
+  def index
+    @cities = City.includes(:state).all
+  end
+
+  def create
+    @city = City.new(city_params)
+      
+    if @city.save 
+      render :create, status: :created
+    else
+      render json: { errors: @city.errors, status: :unprocessed }
+    end
+  end
+
+  private
+
+  def city_params
+    params.require(:city).permit(:name, :state_id)
+  end
+end
